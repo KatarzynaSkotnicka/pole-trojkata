61e823b490dd2071f0324b431fec168b7870d56c
Katarzyna Skotnicka <ka.skotnicka@gmail.com>
2017-12-29 11:29:01 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//zadanie z użyciem prompt
+
+
+var a = prompt('Wpisz wartość podstawy trójkąta');
+var h = prompt('Wpisz wartość wysokości trójkąta'); 
+
+var triangleArea;
+
+triangleArea = a * h / 2;
+
+alert('Pole trójkąta wynosi ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equeal to: ' + triangleArea );
+
+
